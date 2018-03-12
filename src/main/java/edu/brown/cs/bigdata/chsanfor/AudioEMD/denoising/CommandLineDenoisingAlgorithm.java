0d7c6fd6d9141b7deb32f3d81a688205943755d5
John0Randolph <35846969+John0Randolph@users.noreply.github.com>
2018-03-11 22:50:18 -0400

    cmdline denoising abstracted
    Updated & Abstracted CommandLineDenoisingAlgorithm.java & RNNoiseDenoising.java.

--- src/main/java/edu/brown/cs/bigdata/chsanfor/AudioEMD/denoising/CommandLineDenoisingAlgorithm.java
+++ src/main/java/edu/brown/cs/bigdata/chsanfor/AudioEMD/denoising/CommandLineDenoisingAlgorithm.java
@@ -1,6 +1,5 @@
 package edu.brown.cs.bigdata.chsanfor.AudioEMD.denoising;
 
-
 import edu.brown.cs.bigdata.chsanfor.AudioEMD.sequence.AudioSequence;
 
 import java.io.File;
@@ -12,38 +11,98 @@ import java.io.File;
  *  3) Convert the denoised file back into WAV format
  */
 public abstract class CommandLineDenoisingAlgorithm implements DenoisingAlgorithm {
+	
+	/**
+	 * field to store the convertedFileExtension
+	 */
     private String convertedFileExtension;
+    
+    /**
+     * field to store the arguments needed to convert from wav
+     */
+    private String convertArguments;
+    
+    /**
+     * field to store the command line to run the denoiser
+     */
+    private String runCommandLine;
+    
+    /**
+     * field to store the arguments needed to convert back to wav
+     */
+    private String convertBackArguments;
 
     /**
      *
      * @param convertedFileExtension the file extension for the audio that is fed into the denoiser: e.g. "wav", "raw"
+     * @param convertArguments the arguments needed with sox to convert to the file format desired by the denoiser
+     * 		  e.g. " -b 16 -c 1 -r 48k -e unsigned -t raw "
+     * @param runCommandLine the command line to run the denoiser
+     * 		  e.g. "./denoising_algs/rnnoise/examples/rnnoise_demo"
+     * @param convertBackArguments the arguments needed with sox to convert back from to the file format
+     * 		  e.g. "sox -r 48k -b 16 -c 1 -e unsigned "
      */
-    CommandLineDenoisingAlgorithm(String convertedFileExtension) {
+    CommandLineDenoisingAlgorithm(String convertedFileExtension,
+    			String convertArguments, String runCommandLine, String convertBackArguments) {
         this.convertedFileExtension = convertedFileExtension;
+        this.convertArguments = convertArguments;
+        this.runCommandLine = runCommandLine;
+        this.convertBackArguments = convertBackArguments;
     }
 
     /**
      * Converts a WAV file into the needed format for the denoiser
+     * Should call the tryConversion method
      * @param inputFile the input WAV file; obtained from the input AudioSequence
      * @param convertedLocation the (currently empty) file for the input to the denoising algorithm
      *                          (of type convertedFileExtension)
      */
-    public abstract void convertForDenoising(File inputFile, File convertedLocation);
-
+    public void convertForDenoising(File inputFile, File convertedLocation) {
+        try {
+            Process proc = run.exec("sox " + inputFile.getAbsolutePath()
+                    + convertArguments + convertedLocation.getAbsolutePath());
+            // For each command, it's necessary to wait until the procedure completes.
+            // Otherwise, the next command may operate on an empty file.
+            proc.waitFor();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    
     /**
      * Denoises a properly-formatted file
+     * Should call the tryDenoise method
      * @param fileToDenoise the file (of type convertedFileExtension) to input to the denoising algorithm
      * @param denoisedLocation the (currently empty) file for the output of the denoising algorithm
      *                          (of type convertedFileExtension)
      */
-    public abstract void denoise(File fileToDenoise, File denoisedLocation);
+    public void denoise(File fileToDenoise, File denoisedLocation) {
+        try {
+            Process proc = run.exec(runCommandLine
+                    + " " + fileToDenoise.getAbsolutePath()
+                    + " " + denoisedLocation.getAbsolutePath());
+            proc.waitFor();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
     /**
      * Converts the output of the denoising algorithm back into a WAV file
      * @param denoisedOutput the file (of type convertedFileExtension) output by the denoising algorithm
      * @param finalLocation the final WAV file representing the denoised audio
      */
-    public abstract void convertBack(File denoisedOutput, File finalLocation);
+    public void convertBack(File denoisedOutput, File finalLocation) {
+        try {
+            Process proc = run.exec(convertBackArguments
+                    + denoisedOutput.getAbsolutePath() + " "
+                    + finalLocation.getAbsolutePath());
+            proc.waitFor();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
 
     public AudioSequence apply(AudioSequence inputSequence, File outputLocation) {
         File tempInputFile = new File("temp/temp_input." + convertedFileExtension);
--- src/main/java/edu/brown/cs/bigdata/chsanfor/AudioEMD/denoising/RNNoiseDenoisingAlgorithm.java
+++ src/main/java/edu/brown/cs/bigdata/chsanfor/AudioEMD/denoising/RNNoiseDenoisingAlgorithm.java
@@ -13,46 +13,8 @@ public class RNNoiseDenoisingAlgorithm extends CommandLineDenoisingAlgorithm {
 
 
     public RNNoiseDenoisingAlgorithm() {
-        super("raw");
-    }
-
-    @Override
-    public void convertForDenoising(File inputFile, File convertedLocation) {
-        try {
-            Process proc = run.exec("sox " + inputFile.getAbsolutePath()
-                    + " -b 16 -c 1 -r 48k -e unsigned -t raw " + convertedLocation.getAbsolutePath());
-            // For each command, it's necessary to wait until the procedure completes.
-            // Otherwise, the next command may operate on an empty file.
-            proc.waitFor();
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public void denoise(File fileToDenoise, File denoisedLocation) {
-        try {
-            String runCommandLine = "./denoising_algs/rnnoise/examples/rnnoise_demo";
-            Process proc = run.exec(runCommandLine
-                    + " " + fileToDenoise.getAbsolutePath()
-                    + " " + denoisedLocation.getAbsolutePath());
-            proc.waitFor();
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public void convertBack(File denoisedOutput, File finalLocation) {
-        try {
-            Process proc = run.exec("sox -r 48k -b 16 -c 1 -e unsigned "
-                    + denoisedOutput.getAbsolutePath() + " "
-                    + finalLocation.getAbsolutePath());
-            proc.waitFor();
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
-
+        super("raw", " -b 16 -c 1 -r 48k -e unsigned -t raw ", "./denoising_algs/rnnoise/examples/rnnoise_demo", 
+        		"sox -r 48k -b 16 -c 1 -e unsigned ");
     }
 
 }
