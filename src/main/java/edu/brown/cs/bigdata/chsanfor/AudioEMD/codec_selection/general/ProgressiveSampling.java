95d37e7aae6dd3b3276cc68f2bc65a7c0c6690de
Clayton Sanford <clayton_sanford@brown.edu>
2018-03-14 23:23:27 -0400

    fixed dependency

--- src/main/java/edu/brown/cs/bigdata/chsanfor/AudioEMD/codec_selection/general/ProgressiveSampling.java
+++ src/main/java/edu/brown/cs/bigdata/chsanfor/AudioEMD/codec_selection/general/ProgressiveSampling.java
@@ -1,7 +1,5 @@
 package edu.brown.cs.bigdata.chsanfor.AudioEMD.codec_selection.general;
 
-import com.sun.tools.javac.util.ArrayUtils;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
