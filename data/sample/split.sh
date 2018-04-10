95cc5c32b1422078d0de07f653ffb21fe2633356
Cyrus Cousins <originalcyruscousins@gmail.com>
2018-04-10 19:47:22 -0400

    Fixed split script to remove (bugged) support for arbitrary filetypes; now using wav.

--- data/sample/split.sh
+++ data/sample/split.sh
@@ -5,8 +5,7 @@ set -e
 inputdir=$1
 outputdir=$2
 segment_time=$3
-filetype=$4
-name=$5
+name=$4
 
 mkdir -p $outputdir
 
@@ -20,6 +19,7 @@ do
         filename=$(basename "${f}")
         echo "Processing $count of $numfiles: $filename"
         echo "Command: ffmpeg -i $f -f segment -segment_time $segment_time -c copy $outputdir/$name\_$count\_%03d.$filetype -loglevel quiet"
-        ffmpeg -i "$f" -f segment -segment_time $segment_time -c copy $outputdir/$name\_$count\_%03d.$filetype -loglevel quiet
+        ffmpeg -i "$f" -f segment -segment_time $segment_time $outputdir/$name\_$count\_%03d.wav -acodec pcm_s32le -ar 44100 -loglevel quiet
+
 done
 
